6c8a2469a1c7c6fdc94b194c82a8c13453cf2276
nhanbn98 <nhanbn98@gmail.com>
2021-06-17 17:28:56 +0700

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,10 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 export const environment = {
-  production: true,
+  production: false,
+  apiUrl: 'http://localhost:9001/',
+  serverUrl: {
+    // baseUrl: 'http://localhost:9001',
+    baseUrl: 'http://192.168.1.214:9001',
+  },
 };
